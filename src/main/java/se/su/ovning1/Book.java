608ace90afdf4e833faa6fe6aaf95d5d51b13fbd
BerminBear <benjamin.salmonsson@outlook.com>
2026-03-24 14:55:31 +0100

    Lagt till Book, Item och Ej färdig REcording

---
+++ src/main/java/se/su/ovning1/Book.java
@@ -0,0 +1,33 @@
+package se.su.ovning1;
+
+
+public class Book extends Item { //implements PriceableWithVAT6 
+
+    private double price;
+    private boolean bound;
+    private String author;
+    
+    
+    Book(String name, String author, double price, boolean bound){
+        super(name);
+        this.author = author;
+        this.price = price;
+        this.bound = bound;
+    }
+
+
+    public String getType(){
+        return "Book";
+    }
+
+    public double getPrice(){
+        if (bound == true){
+            return price *1.3;
+        }
+        return price;
+    }
+
+    public String toString(){
+        return author;
+    } 
+}
---
+++ src/main/java/se/su/ovning1/Item.java
@@ -0,0 +1,15 @@
+package se.su.ovning1;
+
+public abstract class Item {
+
+    private String name;
+    
+protected Item (String name){
+    this.name = name;
+}
+
+public String getName(){
+     return name;
+}
+
+}
